101c7cddc289a419645331c575d78dd87786728b
Emilio Eiji <emilioeiji@gmail.com>
2022-12-21 13:09:01 +0900

    Ajuste do field data_publicacão para timezone.now

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
-    data_publicacao = models.DateTimeField(default=timezone.now())
+    data_publicacao = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.titulo
