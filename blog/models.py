09af36d90268ac8665fc9a3941d1797662baa4af
Emilio Eiji <emilioeiji@gmail.com>
2023-01-05 12:17:06 +0900

    Estilização do blog - Ajustando as cards e capas dos posts.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
+    resumo = RichTextField(null=True, blank=True)
     conteudo = RichTextUploadingField()
+    image_capa = models.ImageField(null=True, blank=True, upload_to="static/blog/img/")
     data_publicacao = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
