94be0ab2b1859107a5a29752deeecb689cc34ac9
Emilio Eiji <emilioeiji@gmail.com>
2023-01-09 11:15:38 +0900

    Estilizando formulário de contato com modelForms.

--- contato/contato_form.py
+++ contato/contato_form.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from contato.models import Contato
 
 
@@ -6,3 +7,9 @@ class FormContato(ModelForm):
     class Meta:
         model = Contato
         fields = '__all__'
+        widgets = {
+            'assunto': forms.Select(attrs={'class': 'form-control form-select mb-3'}),
+            'nome': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control mb-3'}),
+            'mensagem': forms.Textarea(attrs={'class': 'form-control mb-3'}),
+        }
