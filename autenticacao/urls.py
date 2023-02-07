6731e0f9d3973447816e7af844b56926fe107539
emilioeiji <emilioeiji@gmail.com>
2023-02-07 10:14:09 +0900

    Implementando template para a tela de reset de senha.

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('autenticacao/processa_logout/', views.processa_logout, name='logout'),
     path('autenticacao/processa_redirect_home/', views.processa_redirect_home, name='go_home'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="autenticacao/password_reset_form.html"), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
