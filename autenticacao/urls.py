e4d6acb7cddea49d49e809d35edfb3346afd7a86
emilioeiji <emilioeiji@gmail.com>
2023-02-08 10:47:43 +0900

    Implementando templates para o processo de reset de senha.

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -12,8 +12,20 @@ urlpatterns = [
     path('autenticacao/processa_logout/', views.processa_logout, name='logout'),
     path('autenticacao/processa_redirect_home/', views.processa_redirect_home, name='go_home'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="autenticacao/password_reset_form.html"), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name="autenticacao/password_reset_form.html"),
+         name='password_reset'),
+
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(template_name="autenticacao/password_reset_done.html"),
+         name='password_reset_done'),
+
+    path('password_reset_confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="autenticacao/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+
+    path('password_reset_complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name="autenticacao/password_reset_complete.html"),
+         name='password_reset_complete.html'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
