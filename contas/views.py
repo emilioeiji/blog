650bb5a1c6b2bf804be69884cdf0d18541aff091
emilioeiji <emilioeiji@gmail.com>
2023-02-12 07:27:38 +0900

    Ajuste na validação do cadastro de usuário. Habilitando ou desabilitando o botão submit.

--- contas/views.py
+++ contas/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from . usuario_form import PerfilForm
+from django.template.loader import render_to_string
 
 def criar_conta(request):
     if request.method == 'POST':
@@ -38,26 +39,39 @@ def validou_email(email):
 
 
 def htmx_valida_username(request):
+
+    context = {'error_usrname': 'Usuário não disponível.', 'st_submit': 'disabled', 'cor': 'red'}
     usernameParam = request.POST.get('username')
-    if len(usernameParam) <5 :
-        return HttpResponse("<label style='color:red;'>Tamanho mínimo de 5 caracteres.</label>")
-    elif User.objects.filter(username=usernameParam):
-        return HttpResponse('<label style="color:red;">Usuário Indisponível</label>')
-    else:
-        return HttpResponse('<label style="color:green;">Usuário Disponível</label>')
+
+    if not User.objects.filter(username=usernameParam):
+        context['error_usrname'] = 'Usuário disponível.'
+        context['cor'] = 'green'
+
+    if PerfilForm(request.POST).is_valid():
+        context['st_submit'] = ''
+
+    str_template = render_to_string('contas/feedback_form_validation.html', context)
+    return HttpResponse(str_template)
 
 
 def htmx_valida_senha(request):
+
+    context = {'error_pwd': 'As senhas não coincidem.', 'st_submit': 'disabled', 'cor': 'red'}
+
     pwd_confirm = request.POST.get('pwd_confirm')
     password = request.POST.get('password')
 
-    if pwd_confirm != password:
-        return HttpResponse('<label style="color:red;">Senhas não são iguais.</label>')
-    else:
-        return HttpResponse('')
+    if pwd_confirm == password and PerfilForm(request.POST).is_valid():
+        context['error_pwd'] = ''
+        context['st_submit'] = ''
+
+    str_template = render_to_string('contas/feedback_form_validation.html', context)
+    return HttpResponse(str_template)
 
 
 def htmx_valida_email(request):
+
+    context = { 'email_val': 'Email inválido ou já cadastrado', 'st_submit': 'disabled' }
 
     emailParam = request.POST.get('email')
 
@@ -67,4 +81,7 @@ def htmx_valida_email(request):
     if User.objects.filter(email=emailParam):
         return HttpResponse('<label style="color:red;">Email já cadastrado</label>')
     else:
-        return HttpResponse('')
+        context['email_val'] = 'Email disponível e válido.'
+        context['st_submit'] = ''
+        str_template = render_to_string('contas/feedback_form_validation.html', context)
+        return HttpResponse(str_template)
