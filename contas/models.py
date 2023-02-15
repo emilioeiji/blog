66c274dea824f9262e875c16dc9fe23f615e6bf8
emilioeiji <emilioeiji@gmail.com>
2023-02-15 10:46:15 +0900

    Iniciando associação de perfil com o usuário.

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True, max_length=255)
+    foto = models.ImageField(blank=True, null=True, default='', upload_to='static/contas')
+
+
+@receiver(post_save, sender=User)
+def criar_perfil_usuario(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def salvar_perfil_usuario(sender, instance, **kwargs):
+    instance.perfil.save()
