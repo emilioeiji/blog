1e0f7b546accad7cd5e967b925f6cb454b813d9b
emilioeiji <emilioeiji@gmail.com>
2023-02-20 10:52:31 +0900

    Iniciando o desenvolvimento da aplicação quiz.

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from quiz.models import Questao
+
+
 # Create your views here.
 def get_quiz(request):
-    return render(request, 'quiz/quiz.html')
+    questao = Questao.objects.get(pk=1)
+    context = {
+        'questao': questao
+    }
+    return render(request, 'quiz/quiz.html', context)
