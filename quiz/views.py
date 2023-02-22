efedf21c2da4b6ff3d4ddf8705c87e35750cdbd6
emilioeiji <emilioeiji@gmail.com>
2023-02-22 09:46:45 +0900

    Retornando se a resposta está correta ou não em htmx.

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from quiz.models import Questao
@@ -10,3 +11,13 @@ def get_quiz(request):
         'questao': questao
     }
     return render(request, 'quiz/quiz.html', context)
+
+def get_resposta(request):
+    resposta = request.POST.get('resposta')
+
+    if resposta == 'True':
+        return HttpResponse('<p style="color:green"> Resposta Correta!</p>')
+    elif resposta == 'False':
+        return HttpResponse('<p style="color:red"> Resposta Incorreta! </p>')
+    else:
+        return HttpResponse('<p style="color:yellow"> Selecione uma alternativa! </p>')
