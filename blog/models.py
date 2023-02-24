f5ebb97f89872394f2ba4990828d4f5891e8908f
emilioeiji <emilioeiji@gmail.com>
2023-02-24 12:24:16 +0900

    Finalizando a modelagem do banco de dados e iniciando o refactoring dos templates.

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     subtitulo = models.CharField(max_length=255)
     image_capa = models.ImageField(null=True, blank=True, upload_to="static/blog/img/")
     data_publicacao = models.DateField()
+    tempo_leitura = models.CharField(max_length=2, default=30)
 
     def __str__(self):
         return self.titulo
@@ -19,19 +20,26 @@ class Tag(models.Model):
     nome = models.CharField(max_length=50)
     posts = models.ManyToManyField(Post)
 
+    def __str__(self):
+        return str(self.nome)
+
 class Assunto(models.Model):
     nome = models.CharField(max_length=255)
     posts = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return str(self.nome)
 
 class Situacao(models.Model):
     nome = models.CharField(max_length=50)
     posts = models.ManyToManyField(Post, through='PostSituacao', through_fields=('situacao', 'post'))
 
+    def __str__(self):
+        return str(self.nome)
 
 class PostSituacao(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     situacao = models.ForeignKey(Situacao, on_delete=models.CASCADE)
-    #post_situacao = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_situacao')
     data = models.DateField(null=True, blank=True)
 
     class Meta:
@@ -41,8 +49,13 @@ class Topico(models.Model):
     conteudo = RichTextField(blank=True, null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.conteudo)
 
 class Comentario(models.Model):
     texto = models.TextField(max_length=1024)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return str(self.texto)
